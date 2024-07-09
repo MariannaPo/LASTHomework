87c1e34d3658e42562600de44c18b17bf547ee5e
Marianna_Po <mapodmasko@gmail.com>
2024-07-09 14:33:06 +0300

    filtered friends, correct HW2

--- src/HW/2/HW2.tsx
+++ src/HW/2/HW2.tsx
@@ -10,12 +10,13 @@ export type UserType = {
   id: number;
   name: string;
   age: number;
-  address: any; // пропиши типизацию
+  address: AddressType;
 };
 
 export type UsersObjectType = {
-  myFriends: any; // пропиши типизацию
-};
+myFriends: UserType[],
+}
+
 
 export const HW2 = () => {
 
@@ -44,11 +45,11 @@ export const HW2 = () => {
     ]
   }
 
-  let [currentUsers, setCurrentUsers] = useState<UsersObjectType>(users);
+  let [currentUsers, setCurrentUsers] = useState<UserType[]>(users.myFriends);
 
   const filterUsers = () => {
-    const filteredUsers = 'НУЖНО ПРОФИЛЬТРОВАТЬ ДРУЗЕЙ. ОСТАВЛЯЕМ ТОЛЬКО ТЕХ, КОТОРЫЕ ЖИВУТ В ГОРОДЕ LOS ANGELES';
-    setCurrentUsers({ myFriends: filteredUsers });
+    const filteredUsers = users.myFriends.filter(fr=> fr.address.city === 'Los Angeles')
+    setCurrentUsers(filteredUsers);
   };
 
   return (
